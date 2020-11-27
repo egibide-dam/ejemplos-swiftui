b7d55a1c5e73464baca00a019a1f3e10b23319e5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-11-27 12:15:27 +0100

    Proyecto nuevo tipo SwiftUI

---
+++ EjemplosSwiftUI/EjemplosSwiftUI/EjemplosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemplosSwiftUIApp.swift
+//  EjemplosSwiftUI
+//
+//  Created by Ion Jaureguialzo Sarasola on 27/11/20.
+//
+
+import SwiftUI
+
+@main
+struct EjemplosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
